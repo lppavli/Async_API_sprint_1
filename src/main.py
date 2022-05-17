150b1c53ee325e105496a7ab0b7789c43438f5c3
lppavli <lp.pavli@gmail.com>
2022-05-17 21:45:17 +0300

    Базовая структура создана

--- src/main.py
+++ src/main.py
@@ -21,14 +21,14 @@ app = FastAPI(
 
 @app.on_event('startup')
 async def startup():
-    redis.redis = await aioredis.create_redis_pool((config.REDIS_HOST, config.REDIS_PORT), minsize=10, maxsize=20)
+    #redis.redis = await aioredis.create_((config.REDIS_HOST, config.REDIS_PORT), minsize=10, maxsize=20)
     elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
 
 
 @app.on_event('shutdown')
 async def shutdown():
-    redis.redis.close()
-    await redis.redis.wait_closed()
+    #redis.redis.close()
+    #await redis.redis.wait_closed()
     await elastic.es.close()
 
 
