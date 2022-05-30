ae00d27f23e3aca9186c276ee7370357e13f8278
lppavli <lp.pavli@gmail.com>
2022-05-30 15:18:38 +0300

    Поиск по id для жанров и персон работает из Докер-контейнера

--- src/api/v1/genre.py
+++ src/api/v1/genre.py
@@ -24,4 +24,3 @@ async def genre_details(genre_id: str, genre_service: GenreService = Depends(get
 async def genre_list(page_size: int, page_number: int, genre_service: GenreService = Depends(get_genre_service)):
     genres = await genre_service.get_list(page_number, page_size)
     return genres
-
